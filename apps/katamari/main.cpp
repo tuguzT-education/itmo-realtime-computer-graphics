3e04b91373052600e47a809c568b046bd6e981c5
tuguzT <timurka.tugushev@gmail.com>
2024-05-04 22:21:07 +0300

    Add boat mesh

--- apps/katamari/main.cpp
+++ apps/katamari/main.cpp
@@ -8,7 +8,7 @@ int main() {
     Game game{window, input};
 
     auto& component = game.AddComponent<borov_engine::TriangleComponent>();
-    component.LoadMesh("resources/meshes/tanto/tanto.fbx");
+    component.LoadMesh("resources/meshes/boat/boat.fbx");
     component.Transform().scale /= 100;
 
     game.Run();
