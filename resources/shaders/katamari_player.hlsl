66b1a00ef8b491666e4f56dd908ccc295ab732f3
tuguzT <timurka.tugushev@gmail.com>
2024-05-17 22:42:48 +0300

    Add specular component from directional light

--- resources/shaders/katamari_player.hlsl
+++ resources/shaders/katamari_player.hlsl
@@ -26,16 +26,16 @@ struct VS_Output
 
 VS_Output VSMain(VS_Input input)
 {
-	VS_Output output = (VS_Output)0;
+    VS_Output output = (VS_Output)0;
 
     input.position.xy += 0.25f * cos(2.0f * time);
-	input.position.z *= (sin(time) + 1.0f) * 0.5f;
+    input.position.z *= (sin(time) + 1.0f) * 0.5f;
 
-	output.position = mul(WorldViewProjection(transform), float4(input.position, 1.0f));
-	output.normal = normalize(mul(transform.world, float4(input.normal, 0.0f)).xyz);
-	output.color = input.color;
-	output.texture_coordinate = input.texture_coordinate * tile_count;
-	output.world_position = mul(transform.world, float4(input.position, 1.0f)).xyz;
+    output.position = mul(WorldViewProjection(transform), float4(input.position, 1.0f));
+    output.normal = normalize(mul(transform.world, float4(input.normal, 0.0f)).xyz);
+    output.color = input.color;
+    output.texture_coordinate = input.texture_coordinate * tile_count;
+    output.world_position = mul(transform.world, float4(input.position, 1.0f)).xyz;
 
-	return output;
+    return output;
 }
--- resources/shaders/triangle_component.hlsl
+++ resources/shaders/triangle_component.hlsl
@@ -26,15 +26,15 @@ struct VS_Output
 
 VS_Output VSMain(VS_Input input)
 {
-	VS_Output output = (VS_Output)0;
+    VS_Output output = (VS_Output)0;
 
-	output.position = mul(WorldViewProjection(transform), float4(input.position, 1.0f));
-	output.normal = normalize(mul(transform.world, float4(input.normal, 0.0f)).xyz);
-	output.color = input.color;
-	output.texture_coordinate = input.texture_coordinate * tile_count;
-	output.world_position = mul(transform.world, float4(input.position, 1.0f)).xyz;
+    output.position = mul(WorldViewProjection(transform), float4(input.position, 1.0f));
+    output.normal = normalize(mul(transform.world, float4(input.normal, 0.0f)).xyz);
+    output.color = input.color;
+    output.texture_coordinate = input.texture_coordinate * tile_count;
+    output.world_position = mul(transform.world, float4(input.position, 1.0f)).xyz;
 
-	return output;
+    return output;
 }
 
 Texture2D DiffuseMap : register(t0);
@@ -52,10 +52,15 @@ typedef VS_Output PS_Input;
 
 float4 PSMain(PS_Input input) : SV_Target
 {
-	float4 color = has_texture ? DiffuseMap.Sample(Sampler, input.texture_coordinate) : float4(1.0f, 1.0f, 1.0f, 1.0f);
-	color *= input.color;
+    float4 color = has_texture ? DiffuseMap.Sample(Sampler, input.texture_coordinate) : float4(1.0f, 1.0f, 1.0f, 1.0f);
+    color *= input.color;
 
-	float4 ambient = color * ambient_light.color;
-	float4 diffuse = dot(-directional_light.direction, input.normal) * directional_light.color * color;
-	return saturate(ambient + diffuse);
+    const float3 view_direction = normalize(view_position - input.world_position);
+    const float3 light_direction = normalize(-directional_light.direction);
+    const float3 reflect_direction = reflect(light_direction, input.normal);
+
+    float4 ambient = color * ambient_light.color;
+    float4 diffuse = saturate(dot(light_direction, input.normal) * directional_light.color * color);
+    float4 specular = pow(max(0.0f, dot(-view_direction, reflect_direction)), 32.0f) * 0.3f;
+    return ambient + diffuse + specular;
 }
