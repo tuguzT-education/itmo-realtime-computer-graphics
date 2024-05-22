2163cb210d77737d73eaf9aca2c42a59ac3978a5
tuguzT <timurka.tugushev@gmail.com>
2024-05-22 16:29:29 +0300

    Move lightning calculations into function, fix attenuation issue with point light

--- resources/shaders/triangle_component.hlsl
+++ resources/shaders/triangle_component.hlsl
@@ -53,30 +53,35 @@ cbuffer PSConstantBuffer : register(b0)
 
 typedef VS_Output PS_Input;
 
+float4 PhongLightning(float3 to_light_direction, float4 light_diffuse,
+                      Material material, float3 position, float3 normal)
+{
+    float3 to_view_direction = normalize(view_position - position);
+    float3 reflect_direction = normalize(reflect(to_light_direction, normal));
+
+    float4 diffuse = max(dot(to_light_direction, normal), 0.0f) * light_diffuse * material.diffuse;
+    float4 specular = pow(max(dot(-to_view_direction, reflect_direction), 0.0f), material.exponent) * material.specular;
+    return diffuse + specular;
+}
+
 float4 PSMain(PS_Input input) : SV_Target
 {
     float4 color = has_texture ? DiffuseMap.Sample(Sampler, input.texture_coordinate) : float4(1.0f, 1.0f, 1.0f, 1.0f);
     color *= input.color;
 
-    float3 view_direction = normalize(input.world_position - view_position);
+    float4 d_diffuse_specular = PhongLightning(-directional_light.direction, directional_light.color,
+                                               material, input.world_position, input.normal);
 
-    float3 d_light_direction = normalize(-directional_light.direction);
-    float3 d_reflect_direction = reflect(d_light_direction, input.normal);
-    float4 d_diffuse = saturate(dot(d_light_direction, input.normal) * directional_light.color * material.diffuse * color);
-    float4 d_specular = saturate(pow(max(0.0f, dot(view_direction, d_reflect_direction)), material.exponent) * material.specular);
-
-    float3 p_light_direction = normalize(point_light.position - input.world_position);
-    float3 p_reflect_direction = reflect(p_light_direction, input.normal);
+    float3 p_light_direction = point_light.position - input.world_position;
     float p_distance = length(p_light_direction);
     float4 p_attenuation = point_light.attenuation.const_factor +
                            point_light.attenuation.linear_factor * p_distance +
                            point_light.attenuation.quad_factor * p_distance * p_distance;
-    float4 p_diffuse = saturate(dot(p_light_direction, input.normal) * point_light.color * material.diffuse * color) / p_attenuation;
-    float4 p_specular = saturate(pow(max(0.0f, dot(view_direction, p_reflect_direction)), material.exponent) * material.specular) / p_attenuation;
+    float4 p_diffuse_specular = PhongLightning(normalize(p_light_direction), point_light.color,
+                                               material, input.world_position, input.normal) / p_attenuation;
 
-    float4 ambient = color * ambient_light.color * material.ambient;
-    float4 diffuse = d_diffuse + p_diffuse;
-    float4 specular = d_specular + p_specular;
+    float4 ambient = ambient_light.color * material.ambient;
+    float4 diffuse_specular = d_diffuse_specular + p_diffuse_specular;
     float4 emissive = material.emissive;
-    return saturate(ambient + diffuse + specular + emissive);
+    return color * (ambient + diffuse_specular + emissive);
 }
